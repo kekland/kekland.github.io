d1e2a9bd8c9f32560c46b58e986f49d39e51e943
kekland <kk.erzhan@gmail.com>
2019-08-30 17:45:07 +0600

    Initial commit

---
+++ precache-manifest.042e8d13fda0b59970dcc450b98d63f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3631c041c8108e31c5c9c3b8fa92232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4584a190f491bdd85eba",
+    "url": "/static/css/main.eba46d75.chunk.css"
+  },
+  {
+    "revision": "777ce1b54d02db967e3b",
+    "url": "/static/js/2.e163b34d.chunk.js"
+  },
+  {
+    "revision": "4584a190f491bdd85eba",
+    "url": "/static/js/main.88d58241.chunk.js"
+  },
+  {
+    "revision": "ead1bbdf8ff56073e67d",
+    "url": "/static/js/runtime~main.011e102f.js"
+  }
+]);
